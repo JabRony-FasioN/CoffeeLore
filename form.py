afea8c216644089af7baaab4129a5b3a0394cfb7
JabRony-FasioN <ahiamsorry@mail.ru>
2022-11-13 19:08:24 +0300

    daw

--- form.py
+++ form.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
 
-class data_input(FlaskForm):
+class MessageForm(FlaskForm):
     name = StringField("name:  ", validators=[DataRequired()])
-    last_name = StringField("last_name:  ", validators=[DataRequired()])
+    email = StringField("Email: ", validators=[Email()])
+    message = TextAreaField('Message', validators=[DataRequired()])
+    submit = SubmitField("submit")
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
