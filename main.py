8f832907d00b602e945af0177101836388640b27
JabRony-FasioN <ahiamsorry@mail.ru>
2022-11-14 23:31:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,17 +14,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('former.html')
+    return render_template('index.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = data_input(request.form)
-    if form.validate_on_submit():
-        name = form.name.data
-        last_mame = form.name.data
-        print(name, last_mame)
-        return redirect(url_for('name'))
-    return render_template('former.html', form = form)
+   if request.method == 'POST':
+    register = request.form
+    return render_template('former.html', register = register)
+
+
 if __name__ == '__main__':
     app.run(port=5005) 
 
